4d165654251beb94a5b1eab6fc08b5e90d68bf79
eatissot <tissoted@gmail.com>
2015-08-03 11:04:40 -0400

    Update miniBatch.m

--- eduardo/MATLab/miniBatch.m
+++ eduardo/MATLab/miniBatch.m
@@ -1,4 +1,6 @@
-function [params, J_history] = miniBatch(f,params,X,y,epsilon,alpha,batch_size,epochs)
+ function [params, J_history] = miniBatch(f,params,X,y,epsilon,alpha,batch_size,epochs)
+ %  This is a mini-batch learnibg algorithm with adaptative learning rates
+ %  and momentum
 
 % Setup some useful variables
 m=size(X,1);
@@ -6,6 +8,8 @@ numbatches=floor(m/batch_size);
 J_history=zeros(epochs,1);
 
 Theta_velocity=zeros(size(params));
+local_gain=ones(size(params));
+previous_grad=ones(size(params));
 
 for e=1:epochs
     perm=randperm(m);
@@ -16,8 +20,13 @@ for e=1:epochs
         ybatch=y((b-1)*batch_size+1:b*batch_size,:);
         [J_history(e), Theta_grad]=f(params,Xbatch,ybatch);
         
-        Theta_velocity=alpha.*Theta_velocity+Theta_grad;
-        params=params-epsilon*Theta_velocity;
+        previous_grad=Theta_grad.*previous_grad;
+        local_gain(previous_grad>0)=local_gain(previous_grad>0)+0.05;
+        local_gain(previous_grad<=0)=local_gain(previous_grad<=0).*0.95;
+        previous_grad=Theta_grad;
+        
+        Theta_velocity=alpha.*Theta_velocity-(epsilon*local_gain).*Theta_grad;
+        params=params+Theta_velocity;
     end
     fprintf('Iteration %4i | Cost: %4.6e\r', e, J_history(e));
 end
