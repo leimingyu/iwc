566275b75c32968b6c67ee02debbf41c6fd236c1
eatissot <tissoted@gmail.com>
2015-10-15 20:19:49 -0300

    Update miniBatch.m

--- eduardo/MATLab/miniBatch.m
+++ eduardo/MATLab/miniBatch.m
@@ -4,7 +4,7 @@
 
 % Setup some useful variables
 m=size(X,1);
-numbatches=floor(m/batch_size);
+numbatches=floor(m/batch_size); % Define number of batches
 J_history=zeros(epochs,1);
 
 Theta_velocity=zeros(size(params));
@@ -12,7 +12,7 @@ local_gain=ones(size(params));
 previous_grad=ones(size(params));
 
 for e=1:epochs
-    perm=randperm(m);
+    perm=randperm(m); % Random permutation of the dataset in each epoch
     X=X(perm,:);
     y=y(perm,:);
     for b=1:numbatches
@@ -20,11 +20,14 @@ for e=1:epochs
         ybatch=y((b-1)*batch_size+1:b*batch_size,:);
         [J_history(e), Theta_grad]=f(params,Xbatch,ybatch);
         
+        % Adaptive learning rate: if the previous and actual grads have the same signal the local gain is added to 0.05,
+        % if their signal is not the same, the local gain is multiplied by 0.95.
         previous_grad=Theta_grad.*previous_grad;
         local_gain(previous_grad>0)=local_gain(previous_grad>0)+0.05;
         local_gain(previous_grad<=0)=local_gain(previous_grad<=0).*0.95;
         previous_grad=Theta_grad;
         
+        % Momentum is applied here: alpha is the momentum hyperparameter.
         Theta_velocity=alpha.*Theta_velocity-(epsilon*local_gain).*Theta_grad;
         params=params+Theta_velocity;
     end
